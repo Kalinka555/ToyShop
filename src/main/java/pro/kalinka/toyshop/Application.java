40bbc3af324ba669992d77952216b5c639699631
Alina <K.alinka555@list.ru>
2021-02-27 18:15:38 +0300

    add program status to output

--- src/main/java/pro/kalinka/toyshop/Application.java
+++ src/main/java/pro/kalinka/toyshop/Application.java
@@ -2,6 +2,7 @@ package pro.kalinka.toyshop;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Shop program started...");
         System.out.println("Heloo from our shop!");
     }
 }
