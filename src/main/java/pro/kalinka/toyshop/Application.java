70e63beaae0eea83c185a67f8d320f6f5d022255
Kalinka555 <77123939+Kalinka555@users.noreply.github.com>
2021-02-27 19:29:26 +0300

    Update Application.java
    changes for hithub

--- src/main/java/pro/kalinka/toyshop/Application.java
+++ src/main/java/pro/kalinka/toyshop/Application.java
@@ -3,6 +3,7 @@ package pro.kalinka.toyshop;
 public class Application {
     public static void main(String[] args) {
         // changes for 2
+        // changes for hithub
         System.out.println("Shop program started...");
         System.out.println("Heloo from our shop!");
         System.out.println("Be careful! Bye!");
