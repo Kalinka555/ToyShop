e16ef3a84ccb36756c2347761a149ba7211426a8
Alina <K.alinka555@list.ru>
2021-02-27 18:25:55 +0300

    changes for branch "2"

--- src/main/java/pro/kalinka/toyshop/Application.java
+++ src/main/java/pro/kalinka/toyshop/Application.java
@@ -2,6 +2,7 @@ package pro.kalinka.toyshop;
 
 public class Application {
     public static void main(String[] args) {
+        // changes for 2
         System.out.println("Shop program started...");
         System.out.println("Heloo from our shop!");
         System.out.println("Be careful! Bye!");
