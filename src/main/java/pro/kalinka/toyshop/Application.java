12064dd4e09541c68824323d60ba2c7eb33a1cad
Alina <K.alinka555@list.ru>
2021-02-27 19:34:14 +0300

    changes for w

--- src/main/java/pro/kalinka/toyshop/Application.java
+++ src/main/java/pro/kalinka/toyshop/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // changes for 2
         // changes for hithub
+        // changes for w
         System.out.println("Shop program started...");
         System.out.println("Heloo from our shop!");
         System.out.println("Be careful! Bye!");
