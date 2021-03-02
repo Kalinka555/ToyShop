12eaf669d5d17cfae2c6d7816fae750a7b742a53
Alina <K.alinka555@list.ru>
2021-03-02 14:59:02 +0300

    The Application is corrected

--- src/main/java/pro/kalinka/toyshop/Application.java
+++ src/main/java/pro/kalinka/toyshop/Application.java
@@ -9,27 +9,26 @@ import java.nio.file.Path;
 public class Application {
 
     public static void main(String[] args) throws IOException {
+        final Path PATH = Path.of("C:\\Users\\Alina\\IdeaProjects\\ToyShop\\src\\main\\resources\\ProductRegister(Toys).txt");
+        final Charset CHARSET = StandardCharsets.UTF_8;
+
+
         System.out.println("Shop program started...");
         System.out.println("Heloo from our shop!");
         System.out.println("Be careful! Bye!");
 
-        final Path PATH = Path.of("C:\\Users\\Alina\\IdeaProjects\\ToyShop\\src\\main\\resources\\ProductRegister(Toys).txt");
-        final Charset CHARSET = StandardCharsets.UTF_8;
         String allProductList = Files.readString(PATH, CHARSET);
+        System.out.println("\n Test" + allProductList);
 
-        if (args[0] == "list-products") {
+        if (args.equals("list-products")) {
             System.out.println(allProductList);
-        }
-
-        else if (args[0] == "/?" || args[0] == "-?" || args[0] == "-h" || args[0] == "--help" || args[0] == "?") {
+        } else if (args.equals("/?") || args.equals("-?") || args.equals("-h") || args.equals("--help") || args.equals("?")) {
             System.out.println("Usage:\n" +
                     "  java -jar ToyShop.jar [command] [options]\n" +
                     "  Commands:\n" +
                     "   list-products            Show all available products in the shop\n" +
                     "   /?, -?, -h, --help       Show help and usage information");
-        }
-
-        else System.out.println("Welcome to our online shop");
+        } else System.out.println("Welcome to our online shop");
     }
 }
 
