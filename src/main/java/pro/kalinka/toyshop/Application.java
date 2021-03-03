c25f074b7165f92d8b33fff0faa12db93f5ebeed
Alina <K.alinka555@list.ru>
2021-03-03 07:44:35 +0300

    #3 Application corrected

--- src/main/java/pro/kalinka/toyshop/Application.java
+++ src/main/java/pro/kalinka/toyshop/Application.java
@@ -7,36 +7,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Application {
+    final private static String ProductRegister = "C:\\Users\\Alina\\IdeaProjects\\ToyShop\\src\\main\\resources\\ProductRegister(Toys).txt";
+    final private static Charset CHARSET = StandardCharsets.UTF_8;
+    final private static Path PATH = Path.of(ProductRegister);
+
+    final private static String COMMAND_LIST_PRODUCT = "list-products";
+    final private static String COMMAND_QUESTION = "?";
+    final private static String COMMAND_SLASH_QUESTION = "/?";
+    final private static String COMMAND_MINUS_QUESTION = "-?";
+    final private static String COMMAND_MINUS_H = "-h";
+    final private static String COMMAND_MINUS_HELP = "--help";
+
+    final private static String WELCOME_TEXT = "Welcome to our online shop";
+    final private static String USAGE_TEXT = "Usage:\n" +
+            "  java -jar ToyShop.jar [command] [options]\n" +
+            "  Commands:\n" +
+            "   list-products            Show all available products in the shop\n" +
+            "   /?, -?, -h, --help       Show help and usage information";
+
 
     public static void main(String[] args) throws IOException {
 
-        final Path PATH = Path.of("C:\\Users\\Alina\\IdeaProjects\\ToyShop\\src\\main\\resources\\ProductRegister(Toys).txt");
-        final Charset CHARSET = StandardCharsets.UTF_8;
         final String allProductList = Files.readString(PATH, CHARSET);
 
         if (args.length == 0) {
-            System.out.println("Usage:\n" +
-                    "  java -jar ToyShop.jar [command] [options]\n" +
-                    "  Commands:\n" +
-                    "   list-products            Show all available products in the shop\n" +
-                    "   /?, -?, -h, --help       Show help and usage information");
+            System.out.println(USAGE_TEXT);
         } else {
             String arg = args[0];
-            if (args[0].equals("list-products")) {
+            if (arg.equals(COMMAND_LIST_PRODUCT)) {
                 System.out.println(allProductList);
-            } else if (args[0].equals("/?") || arg.equals("-?") || args.equals("-h") || arg.equals("--help") || arg.equals("?")) {
-                System.out.println("Usage:\n" +
-                        "  java -jar ToyShop.jar [command] [options]\n" +
-                        "  Commands:\n" +
-                        "   list-products            Show all available products in the shop\n" +
-                        "   /?, -?, -h, --help       Show help and usage information");
-            } else System.out.println("Welcome to our online shop");
+            } else if (arg.equals(COMMAND_QUESTION) || arg.equals(COMMAND_SLASH_QUESTION) || args.equals(COMMAND_MINUS_QUESTION)
+                    || args.equals(COMMAND_MINUS_HELP) || arg.equals(COMMAND_MINUS_H)) {
+                System.out.println(USAGE_TEXT);
+            } else System.out.println(WELCOME_TEXT);
         }
     }
 }
-
-
-
-
-
-
